b4767afd09d855423980e25ba823a97d35ca13d5
Nicky Marino <nsmarino96@gmail.com>
2017-08-28 21:38:49 -0400

    Add tweeter

--- greyhouse_status.py
+++ greyhouse_status.py
@@ -1,17 +1,19 @@
 import random
-
-try:
-    import greyhouse_status_keys
-except ImportError:
-    print('greyhouse_status_keys.py not found')
-    exit()
+import twitter
+from boto.s3.connection import S3Connection
+s3 = S3Connection(os.environ['CONSUMER_KEY'], 
+                  os.environ['CONSUMER_SECRET'],
+                  os.environ['ACCESS_TOKEN_KEY'],
+                  os.environ['ACCESS_TOKEN_SECRET'])
 
 if __name__ == '__main__':
     # All possible responses to Tweet
-    responses = ['No',
-                'Nope',
-                'Don\'t even bother',
-                'Wait there\'s one! \n...Oh someone just sat down',
+    responses = ['No.',
+                'Nope.',
+                'Don\'t even bother.',
+                'Wait there\'s one! ...Oh someone just sat down :/',
+                '-1',
+                'Well, there\'s three tables with only one person sitting at each.',
     ]
 
     # Attempt to read history
@@ -30,15 +32,25 @@ if __name__ == '__main__':
     next_response = random.choice(responses)
     
     # Send tweet
-    print(next_response)
+    print('Sending tweet: {}'.format(next_response))
+    api = twitter.Api(consumer_key=CONSUMER_KEY,
+                      consumer_secret=CONSUMER_SECRET,
+                      access_token_key=ACCESS_TOKEN_KEY,
+                      access_token_secret=ACCESS_TOKEN_SECRET)
+    try:
+        status = api.PostUpdate(next_response)
+        print('Sent. Status: {}'.format(status))
+    except Exception as ex:
+        print('Tweet could not be sent. Error below:\n{}'.format(ex))
 
     # Only keep a limited amount of history in the log
-    if len(prev_responses) >= 3:
-        log_data = [next_response] + prev_responses[:-1]
+    num_responses_stored = 3
+    if len(prev_responses) >= num_responses_stored:
+        log_data = [next_response] + prev_responses[0:(num_responses_stored-1)]
     else:
         log_data = [next_response] + prev_responses
 
     # 
     with open('greyhouse_status.log', 'w') as out:
         out.write('# Last three responses (newest first)\n')
-        out.write('\n'.join([]))
+        out.write('\n'.join(log_data))
